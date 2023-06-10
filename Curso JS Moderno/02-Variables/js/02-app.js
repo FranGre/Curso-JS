33f9a86e4003d2f299dcba90744c0bbe7d07b891
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-10 23:48:26 +0200

    Variable let(nuevo estandar de var) & Variable const(constantes asignar valor misma linea, can't reasignar)

---
+++ Curso JS Moderno/02-Variables/js/02-app.js
@@ -0,0 +1,6 @@
+let pago = 100;
+let cambio  = 30;
+
+pago = 'El cliente ya ha pagado'
+//let es la nueva version de var
+pago = null
---
+++ Curso JS Moderno/02-Variables/js/03-app.js
@@ -0,0 +1,13 @@
+//const
+//constantes valor obligado en la misma linea y no se pueden reasignar
+
+const iva = 21;
+
+
+/*
+Esto es un error
+
+const descuento;
+
+descuento = 10;
+*/
