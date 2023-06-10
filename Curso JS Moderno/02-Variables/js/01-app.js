8c13e7352c10637235bc02a1d63964a53ccba95c
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-10 23:31:53 +0200

    Variable var(JS antiguo)

---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,8 @@
+var productoJorge = "Monitor de 24 pulgadas",
+productoFran = "Monitor de 21 pulgadas";
+
+console.log(productoJorge);
+console.log(productoFran)
+
+productoFran = 'Monitor averiado'
+console.log(productoFran)
