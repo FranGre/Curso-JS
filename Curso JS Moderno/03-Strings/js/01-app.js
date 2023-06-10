6a28be312a6597b074b841bac857109e61ecfd43
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-11 00:06:23 +0200

    Ways to create a String

---
+++ Curso JS Moderno/03-Strings/js/01-app.js
@@ -0,0 +1,13 @@
+//1ra forma de crear String y más COMUN
+const productoFran = 'TV de 49 "'
+productoFran = "TV de 49 \""
+
+//2da forma de crear String
+const productoJorge = String("GTX 1050")
+
+//3ra forma de crear String
+const productoXavi = new String("Telefono movil")
+
+console.log(productoFran)
+console.log(productoJorge)
+console.log(productoXavi)
