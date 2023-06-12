dc0661ed7821f5ce9599ffbce5810de6604c53dd
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-12 22:38:05 +0200

    String methods (repeat & split)

---
+++ Curso JS Moderno/03-Strings/js/06-app.js
@@ -0,0 +1,10 @@
+// .repeat te va permitir duplicar las veces que quiera por param
+
+let nombre = 'fran '.repeat(3)
+console.log(`${nombre}`) // 3 veces
+// si le pasa por parámetro un double, lo que hace es redondear
+
+
+// .split() desmenuzar un String
+let asignaturas = "ED,PROG,BD"
+console.log(asignaturas.split(","))
