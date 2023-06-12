75831b5cfd711f82baa093ae00cc07d89b24f183
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-12 22:46:22 +0200

    String methods (toLowerCase() & toUpperCase())

---
+++ Curso JS Moderno/03-Strings/js/07-app.js
@@ -0,0 +1,7 @@
+let nombreCompleto = "Fran Gregori Tandazo"
+
+console.log(nombreCompleto)
+// Pasar el string a todo minúscula
+console.log(nombreCompleto.toLowerCase())
+// Pasar el string a todo MAYUSCULA
+console.log(nombreCompleto.toUpperCase())
