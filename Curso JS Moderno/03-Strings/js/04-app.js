1672518171c64d90a3b10a38a7738b80af2b9333
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-12 20:50:38 +0200

    Remove blankSpaces

---
+++ Curso JS Moderno/03-Strings/js/04-app.js
@@ -0,0 +1,15 @@
+let portatil = '  Mac Book  '
+
+console.log(portatil)
+//Elimina espacios del principio
+portatil = portatil.trimStart()
+console.log(portatil)
+
+//Elimina espacios del final
+portatil = portatil.trimEnd()
+console.log(portatil)
+
+//Elimina espacios de ambos lados
+portatil = '  Mac Book '
+portatil = portatil.trim()
+console.log(portatil)
