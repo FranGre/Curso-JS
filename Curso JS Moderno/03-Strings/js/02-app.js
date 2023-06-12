2fc65096164b63134cf4082d3f79c274422e3b46
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-12 20:13:40 +0200

    String methods(length & indexOf & includes)

---
+++ Curso JS Moderno/03-Strings/js/02-app.js
@@ -0,0 +1,8 @@
+let nombre = "Jorge Juan"
+
+console.log(nombre +" = "+ nombre.length+" letras")
+
+console.log(nombre.indexOf("Juan"))
+
+console.log(nombre.includes("Juan"))
+console.log(nombre.includes("juan"))
