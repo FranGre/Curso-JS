348f145d9dbdcef3fd009b523da8efcbf8226ff5
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-12 21:38:10 +0200

    String methods(replace & slice & substring)

---
+++ Curso JS Moderno/03-Strings/js/05-app.js
@@ -0,0 +1,20 @@
+let monitor = "Monitor de 20 pulgadas"
+
+console.log(monitor)
+//queremos cambiar pulgadas por Pulgadas
+
+monitor = monitor.replace("pulgadas","Pulgadas")
+console.log(monitor)
+
+// Queremos cortar la cadena, tenemos dos formas 
+console.log(monitor.slice(7,monitor.length))
+console.log(monitor.slice(5))
+console.log(monitor.charAt(5))
+
+
+console.log(monitor.slice(2,1))
+
+console.log(monitor.substring(7,monitor.length))
+console.log(monitor.substring(5))
+
+console.log(monitor.substring(2,1))
