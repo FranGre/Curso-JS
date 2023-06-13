a957b082e89c32f8538462a2b6625395ca6a1988
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-13 17:51:33 +0200

    Operadores básicos

---
+++ Curso JS Moderno/04-Numeros/js/02-app.js
@@ -0,0 +1,25 @@
+let numeroA = 30
+let numeroB = 20
+
+
+// Sumar
+let resultado = numeroA + numeroB
+console.log(`SUMA: ${resultado}`)
+
+// Restar
+resultado = numeroA - numeroB
+console.log(`RESTA: ${resultado}`)
+
+// Multiplicar
+resultado = numeroA * numeroB
+console.log(`MULTIPLICACION: ${resultado}`)
+
+// Dividir
+resultado = numeroA / numeroB
+console.log(`DIVISION: ${resultado}`)
+
+// Módulo
+resultado = numeroA % numeroB
+console.log(`MODULO: ${resultado}`)
+
+// Si ejecutamos Math en la consola del navegador, veremos todos los métodos de Math
