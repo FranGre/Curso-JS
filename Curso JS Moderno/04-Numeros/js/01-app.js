95753ba53450572dc604467062730102ef5cf897
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-13 17:31:47 +0200

    Ways to declare numbers

---
+++ Curso JS Moderno/04-Numeros/js/01-app.js
@@ -0,0 +1,15 @@
+// crear números
+let edadFran = 21;
+
+edadFran = 21.5
+edadFran = .5
+
+// no es la forma más común
+let numero = new Number(50)
+
+// esto no es número es un String
+let edadJorge = "21"
+
+console.log(edadFran)
+console.log(numero)
+console.log(edadJorge)
