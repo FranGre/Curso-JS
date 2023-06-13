2a40a3e6abb99900147151a1534012773398faaf
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-13 19:21:32 +0200

    Operadores > & <

---
+++ Curso JS Moderno/05-Operadores/js/01-app.js
@@ -0,0 +1,9 @@
+let numero1 = 20
+let numero2 = "20"
+let numero3 = 30
+
+// Operador > Mayor
+console.log(`${numero1} es > Mayor que ${numero3} : ${numero1>numero3}`)
+
+// Operador < Menor
+console.log(`${numero1} es < Menor que ${numero3} : ${numero1<numero3}`)
