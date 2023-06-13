9cdf3821b0769006c650fa75c09269e8ca0c5537
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-13 18:37:14 +0200

    Incrementar o Decrementar Números

---
+++ Curso JS Moderno/04-Numeros/js/05-app.js
@@ -0,0 +1,9 @@
+let vidasGato = 7;
+
+console.log(`Vidas tiene un gato : ${vidasGato}`)
+
+--vidasGato
+console.log(`El gato muere, ahora tiene : ${vidasGato} vidas`)
+
+vidasGato -= 3
+console.log(`El gato muere 3 veces más, ahora tiene : ${vidasGato} vidas`)
