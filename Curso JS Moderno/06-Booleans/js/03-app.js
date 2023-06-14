88618289b5b4631e7484c6c7721a45cfdeb0d219
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-14 18:08:36 +0200

    Buenas practicas al comparar booleans

---
+++ Curso JS Moderno/06-Booleans/js/03-app.js
@@ -0,0 +1,23 @@
+let soyMayorEdad = true;
+let soyBuenoMemorizando = false
+
+// asi se hace un if con booleanos
+
+if (soyMayorEdad){
+    console.log("Soy mayor de edad, puedo beber")
+}else{
+    console.log("Soy mayor de edad, no puedo beber")
+}
+
+// Alternativa usar la ternaria
+// Después del ? true : false
+console.log(soyBuenoMemorizando ? 'No copio en el examen' : 'Copio en el examen')
+
+// Evitar esto por favor
+/**
+ * if (soyMayorEdad == true){
+    console.log("Soy mayor de edad, puedo beber")
+   }else{
+    console.log("Soy mayor de edad, no puedo beber")
+}
+ */
