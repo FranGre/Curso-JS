73fb86a8cd16486b635feb68164a0d7884e5d094
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-14 18:32:36 +0200

    Create Obj Way Obj literal

---
+++ Curso JS Moderno/07-Objetos/js/01-app.js
@@ -0,0 +1,15 @@
+// Crear un objeto Monitor y dentro sus Atributos
+
+// Esto de crear un obj con las llaves se llama Object Literal
+
+// En las variables se usa =
+// En un objeto se usan los :
+
+// El objeto monitor posee 3 llaves / atributos
+let monitor = {
+    nombre: "Monitor 4K 21 pulgadas",
+    precio: 499,
+    disponible: true
+}
+
+console.log(monitor)
