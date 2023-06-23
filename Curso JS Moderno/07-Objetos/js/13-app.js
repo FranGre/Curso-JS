f98bb840085cca11669ddb2b66046f4ae03b6492
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-23 18:52:12 +0200

    Get keys || values || both from object

---
+++ Curso JS Moderno/07-Objetos/js/13-app.js
@@ -0,0 +1,16 @@
+const portatil = {
+  marca: "Apple",
+  modelo: "Macbook Pro 15",
+  anyo: "2022",
+  color: "azul medianoche",
+  precio: 1300,
+};
+
+// Devuelve un array con las llaves
+console.log(Object.keys(portatil));
+
+// Devuelve los valores de las llaves
+console.log(Object.values(portatil));
+
+// Devuelve todo en forma de arrays
+console.log(Object.entries(portatil));
