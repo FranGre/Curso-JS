7c9be977a197d455218b0496308afb5e30455b03
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-23 19:39:58 +0200

    Add value to first or last postion in array

---
+++ Curso JS Moderno/08-Arrays/js/05-app.js
@@ -0,0 +1,14 @@
+// Add values best way
+
+"use strict";
+
+const meses = ["Enero", "Febrero", "Marzo"];
+console.table(meses);
+
+// Add value to end position + 1 of array
+meses.push("Abril");
+console.table(meses);
+
+// Add valiue to first position of array
+meses.unshift("Diciembre");
+console.table(meses);
