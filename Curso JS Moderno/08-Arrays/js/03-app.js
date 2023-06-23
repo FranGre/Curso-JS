7bb7ae826854eb47cf5193ed515e0173bc831bf4
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-23 19:24:13 +0200

    Reccorer Array with for

---
+++ Curso JS Moderno/08-Arrays/js/03-app.js
@@ -0,0 +1,11 @@
+// Recorrer un Array
+
+const meses = ["Enero", "Febrero", "Marzo", "Abril"];
+
+console.table(meses);
+
+// Bucle for
+
+for (let i = 0; i < meses.length; i++) {
+  console.log(meses[i]);
+}
