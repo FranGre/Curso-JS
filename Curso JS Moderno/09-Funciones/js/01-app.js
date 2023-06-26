945e0b3febbc416bf4581ee9609b9b46403f540b
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-26 19:05:51 +0200

    Declarar una funcion

---
+++ Curso JS Moderno/09-Funciones/js/01-app.js
@@ -0,0 +1,16 @@
+// Declaracion de function (Function Declaration)
+
+// Definir una función
+function sumar(a, b) {
+  return a + b;
+}
+
+// Llamar a la función
+console.log(sumar(2, 2));
+
+// Expresion de funcion o Function expression
+const resta = function (a, b) {
+  return a - b;
+};
+
+console.log(resta(2, 2));
