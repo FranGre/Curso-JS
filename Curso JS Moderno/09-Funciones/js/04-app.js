6faf508562eb15b270fd362f328448926e8b2085
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-27 17:35:05 +0200

    Diferencias entre funcion y método

---
+++ Curso JS Moderno/09-Funciones/js/04-app.js
@@ -0,0 +1,10 @@
+// Diferencia entre Funciones y Métodos
+
+const a = 20;
+const b = "20";
+
+// Su diferencia se basa en el contexto donde son utilizadas
+
+console.log(parseInt(b)); // Función
+
+console.log(a.toString()); // Método
