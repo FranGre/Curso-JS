f90a6f3df05615f4bccd0f0aa9fd3a669ab40bfb
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-27 17:43:32 +0200

    Que son los parámetros->sumar(a,b) y argumentos -> sumar(2,3)

---
+++ Curso JS Moderno/09-Funciones/js/05-app.js
@@ -0,0 +1,15 @@
+// argumentos y parámetros
+
+function sumar(a, b) {
+  // a y b son parámetros
+  return a + b;
+}
+
+let resultado = sumar(2, 4); // 2 y 4 son los argumentos
+console.log(resultado);
+
+function saludar(nombre, apellido) {
+  console.log(`Hola ${nombre} ${apellido} !!!`);
+}
+
+saludar("Fran", "Gregori");
